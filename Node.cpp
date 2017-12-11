8ae575e396e6f82621f34c67d45df0c5b9f5545d
Rory37 <292270@bsd48.org>
2017-12-11 12:13:35 -0800

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,23 +1,23 @@
 #include "Node.h"
 
-Node :: Node(Student* newStudent) {
-  next = NULL;
-  student = newStudent;
+Node :: Node(Student* newStudent) { //constructor
+  next = NULL; //defaults next null to be null
+  student = newStudent; //creates the student which is contained in the node
 }
 
-Node :: ~Node() {
-  delete student;
-  next = NULL;
+Node :: ~Node() { //destructor
+  delete student; //gets rid of stored student
+  next = NULL; //sets what it was pointing to to null
 }
 
-void Node :: setNext(Node* newNext) {
-  next = newNext;
+void Node :: setNext(Node* newNext) {//to set the next node in the list
+  next = newNext; 
 }
 
-Node* Node :: getNext() {
+Node* Node :: getNext() { //returns next node in the list
   return next;
 }
 
-Student* Node :: getStudent() {
+Student* Node :: getStudent() { //returns the student
   return student;
 }

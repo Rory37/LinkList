ce79d3a1536a76daa2f5b58821fc2cf7013fe990
Rory37 <292270@bsd48.org>
2017-12-11 12:15:38 -0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,21 +1,17 @@
-//#ifndef GUARD
-//#define GUARD
 #include "Student.h"
 #include <iostream>
 
 using namespace std;
 
 class Node{
- public:
-  Node(Student*);
-  ~Node();
-  Node* getNext();
-  void setNext(Node*);
-  Student* getStudent();
+ public: //declares public functions
+  Node(Student*); //constructor
+  ~Node(); //destructor
+  Node* getNext(); //gets the next in list
+  void setNext(Node*);//sets the next in list
+  Student* getStudent(); //gets the stored student
 
  private:
-  Node* next;
-  Student* student;
+  Node* next; //stores what node is next in the list
+  Student* student;// stores what student is in the node
 };
-
-//#endif

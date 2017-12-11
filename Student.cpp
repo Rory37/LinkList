2e04539bd1a3667524ec212474b2efcd00ddecb2
Rory37 <292270@bsd48.org>
2017-12-11 12:17:49 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Student.h"
-#include <cstring>
+#include <cstring> //for copy
 
 
-void Student :: setName(char* namen) {
-  name = new char[80];
-  strcpy(name, namen);
+void Student :: setName(char* namen) { //will set the student's name
+  name = new char[80]; //sets name as a new pointer to char [80]
+  strcpy(name, namen); //gives the pointer something to point to
 }
 
-char* Student :: getName(){
-  return name;
+char* Student :: getName(){ //Returns student's name
+  return name; 
 }
